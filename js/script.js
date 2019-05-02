eeab87a98e1a3209eac6f4f3245112871d19fad5
mizzta <ritatung@hotmail.com>
2019-05-02 16:46:07 -0700

    image is working.

--- js/script.js
+++ js/script.js
@@ -8,18 +8,16 @@
         $.ajax({
             method: 'get',
             url: "https://api.nytimes.com/svc/topstories/v2/"+selected+".json?api-key=cdvAxEuArJf8OPJNnLPar0Wf2k6Vd8Nd&count=12",
-            // results: Array,
-            // num_results: 12,
           })
             .done(function(data){
             console.log('Success');
             console.log(data.results);
             $.each(data.results, function(index, object){
               console.log(object);
-              // try to append things like object.title, object.
               $('.storycontentarea').append(`
-                <p>${object.title, object.abstract, object.multimedia[4].url}</p>
-                // try writing your html here
+              <p>${object.title}</p>
+              <p>${object.abstract}</p>
+              <img src= "${object.multimedia[4].url}">
               `);
             });
           })
