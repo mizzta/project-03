3ca8f931b2c9bede07101a9c42442bf1294d257e
mizzta <ritatung@hotmail.com>
2019-05-01 23:21:13 -0700

    add js, ajax.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,45 @@
-function marioJump(){
-    console.log("Mario has jumped and squashed a turtle");
+function loadArticles(category){
+    console.log(category);
+$.ajax({
+    method: 'get',
+    url: "https://api.nytimes.com/svc/topstories/v2/"+category+".json?api-key=cdvAxEuArJf8OPJNnLPar0Wf2k6Vd8Nd" 
+  })
+    .done(function(data){
+    console.log('Success!!!');
+    console.log(data);
+  })
+    .fail(function(){
+    console.log('Oops!!!!! ');
+  })
+    .always(function(){
+    $('.loading').hide()
+    })
+
 }
-marioJump();
 
-console.log('test');
+  $('#dropdown-menu').on('change', function(){
+    const selected = $(this).val();
 
-console.log('hello');
+    if (selected !== '') {
+        console.log('The value you picked is: ' + selected);
+        loadArticles(selected);
+        $(".storycontentarea").append("https://api.nytimes.com/svc/topstories/v2/{section}.json".context);
+        $.ajax({
+            method: 'post',
+            url: "https://api.nytimes.com/svc/topstories/v2/"+ selected +".json?api-key=cdvAxEuArJf8OPJNnLPar0Wf2k6Vd8Nd" 
+
+          })
+            .done(function(data){
+            console.log('Success');
+            console.log(data);
+          })
+            .fail(function(){
+            console.log("Oh No! There's something wrong");
+          })
+            .always(function(){
+            $('.loading').hide()
+            })
+  
+
+      }
+    });
