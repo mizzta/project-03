7cac5fae1dee5a9c8b83d8fda74b3380ba8c2f57
mizzta <ritatung@hotmail.com>
2019-05-03 14:27:26 -0700

    move fonts and image to assets folder. add _fonts.scss. add a script line to clear the web content before change category.

--- js/script.js
+++ js/script.js
@@ -5,11 +5,13 @@
         console.log('The value you picked is: ' + selected);
         $.ajax({
             method: 'get',
-            url: "https://api.nytimes.com/svc/topstories/v2/"+selected+".json?api-key=cdvAxEuArJf8OPJNnLPar0Wf2k6Vd8Nd&count=12",
+            url: "https://api.nytimes.com/svc/topstories/v2/"+selected+".json?api-key=cdvAxEuArJf8OPJNnLPar0Wf2k6Vd8Nd",
           })
             .done(function(data){
             console.log('Success');
             console.log(data.results);
+            $('.storycontentarea').html(''); // clear the content
+            // add .filter
             $.each(data.results, function(index, object){
               console.log(object);
               $('.storycontentarea').append(`
