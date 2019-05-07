39746c38b5eaffdbf3904ffe6fb40a8c22e06dd4
mizzta <ritatung@hotmail.com>
2019-05-06 22:16:34 -0700

    trying to add grid.

--- js/script.js
+++ js/script.js
@@ -1,33 +1,44 @@
-  $('#dropdown-menu').on('change', function(){
-    const selected = $(this).val();
+$('#dropdown-menu').on('change', function () {
+  const selected = $(this).val();
 
-    if (selected !== '') {
-        console.log('The value you picked is: ' + selected);
-        $.ajax({
-            method: 'get',
-            url: "https://api.nytimes.com/svc/topstories/v2/"+selected+".json?api-key=cdvAxEuArJf8OPJNnLPar0Wf2k6Vd8Nd",
-          })
-            .done(function(data){
-            console.log('Success');
-            console.log(data.results);
-            $('.storycontentarea').html(''); // clear the content
-            // add .filter
-            $.each(data.results, function(index, object){
-              console.log(object);
-              $('.storycontentarea').append(`
+  if (selected !== '') {
+    console.log('The value you picked is: ' + selected);
+    $.ajax({
+      method: 'get',
+      url: "https://api.nytimes.com/svc/topstories/v2/" + selected + ".json?api-key=cdvAxEuArJf8OPJNnLPar0Wf2k6Vd8Nd",
+    })
+      .done(function (data) {
+        console.log('Success');
+        console.log(data.results);
+        $('.storycontentarea').html(''); // clear the content
+        // add .filter
+        // const result = data.filter(multimedia[4].url != "" );
+        // console.log(result);
+
+        // const newsFiltered = results.filter(function(data){
+        //   return object.multimedia.url !==  undefined;
+        // });
+
+        $.each(data.results, function (index, object) {
+
+          console.log(object);
+          $('.storycontentarea').append(`
               <div class="article">
-              <p class="description">${object.abstract}</p>
-              <img src="${object.multimedia[4].url}"></div>
+              <figure>
+              <img src="${object.multimedia[4].url}">
+              <figcaption>${object.abstract}</figcaption>
+              </figure>
+              </div>
               `);
-            });
-          })
-            .fail(function(){
-            console.log("Oh No! There's something wrong");
-          })
-            .always(function(){
-            $('.loading').hide()
-            })
-  
+        });
+      })
+      .fail(function () {
+        console.log("Oh No! There's something wrong");
+      })
+      .always(function () {
+        $('.loading').hide()
+      })
 
-      }
-    });// end of change event
+
+  }
+});// end of change event
