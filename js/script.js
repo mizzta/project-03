e4d5a3bcbd69166560d61f2034d75e70c98a35d4
mizzta <ritatung@hotmail.com>
2019-05-12 03:18:36 -0700

    add animation

--- js/script.js
+++ js/script.js
@@ -10,9 +10,9 @@ $('#dropdown-menu').on('change', function () {
       url: "https://api.nytimes.com/svc/topstories/v2/" + selected + ".json?api-key=cdvAxEuArJf8OPJNnLPar0Wf2k6Vd8Nd",
     })
       .done(function (data) {
-        const newsFiltered= data.results.filter(function(stories){
-          return stories.multimedia[4] !==  undefined;
-        }).slice(0,12);
+        const newsFiltered = data.results.filter(function (stories) {
+          return stories.multimedia[4] !== undefined;
+        }).slice(0, 12);
         console.log('Success');
         console.log(data.results);
         $('.storycontentarea').html(''); // clear the content
@@ -26,16 +26,20 @@ $('#dropdown-menu').on('change', function () {
               </div>
               `);
         });
-        //   $('storycontentarea').css('background-image', 'url('${object.multimedia[4].url}')');
       })
       .fail(function () {
         console.log("Oh No! There's something wrong");
       })
       .always(function () {
-        $('.loading').hide()
+        $(".loading").show().delay(500).fadeOut();
       })
-
-
   }
-  
+
 });// end of change event
+
+// Newspaper Image
+$('.documents').ready(function () {
+  $('.sayhi').click(function () {
+    $('.hello').fadeOut(1500);
+  });
+});
